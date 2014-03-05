62abdf60bfbc254185ffa2193fc43a1d4cc0fc47
miken <miken@partyrentalltd.com>
2014-03-04 20:54:32 -0500

    aermeabi so libs added as a jar, project converted to gradle build system.  TODO: remove jars that exist on maven central from libs directory

---
+++ app/build/source/buildConfig/debug/edu/sfsu/cs/orange/ocr/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.sfsu.cs.orange.ocr;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "edu.sfsu.cs.orange.ocr";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 33;
+}
